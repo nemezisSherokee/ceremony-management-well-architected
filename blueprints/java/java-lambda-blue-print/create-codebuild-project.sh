10c92abb347df320ea0dc25774263361ddb9bad1
JOSEPH HERMANN KAMDOUM NGADJUI <kamdoum_ngadjui@siemens.com>
2021-08-11 10:58:11 +0200

    adding some change to update the process

--- blueprints/java/java-lambda-blue-print/create-codebuild-project.sh
+++ blueprints/java/java-lambda-blue-print/create-codebuild-project.sh
@@ -1,6 +1,7 @@
 set -eu -o pipefail
 
-STACK_NAME=$(pwd |  awk -F "/" '{print $NF}')
+STACK_NAME=$(pwd |  awk -F "/" '{print $NF}')-cicd
+PROJECT_NAME=$(pwd |  awk -F "/" '{print $NF}')
 echo "creating following buildproject and stack : $STACK_NAME"
 
 echo "Checking if stack exists ..."
@@ -12,7 +13,7 @@ if ! aws cloudformation describe-stacks  --stack-name $STACK_NAME ; then
 
   echo -e "\nStack does not exist, creating ..."
 aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://../../../cicd/codebuildproject.yaml \
---parameters  ParameterKey=PathToConsider,ParameterValue=blueprints/java/java-lambda-blue-print	ParameterKey=ProjectName,ParameterValue=$STACK_NAME \
+--parameters  ParameterKey=PathToConsider,ParameterValue=blueprints/java/java-lambda-blue-print	ParameterKey=ProjectName,ParameterValue=$PROJECT_NAME \
                                 --capabilities CAPABILITY_NAMED_IAM 
 
  
@@ -20,7 +21,7 @@ else
 
   echo "Waiting for stack update to complete ..."
 aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://../../../cicd/codebuildproject.yaml \
---parameters  ParameterKey=PathToConsider,ParameterValue=blueprints/java/java-lambda-blue-print	ParameterKey=ProjectName,ParameterValue=$STACK_NAME \
+--parameters  ParameterKey=PathToConsider,ParameterValue=blueprints/java/java-lambda-blue-print	ParameterKey=ProjectName,ParameterValue=$PROJECT_NAME \
                                 --capabilities CAPABILITY_NAMED_IAM 
  
 
