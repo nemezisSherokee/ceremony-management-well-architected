0d8772cf8d8e34124cd49fa1661218ea89fc416a
JOSEPH HERMANN KAMDOUM NGADJUI <kamdoum_ngadjui@siemens.com>
2021-08-11 10:58:11 +0200

    adding some change to update the process

--- blueprints/java/java-lambda-blue-print/2-deploy.sh
+++ blueprints/java/java-lambda-blue-print/2-deploy.sh
@@ -19,4 +19,4 @@ then
   # gradle build -i
 fi
 aws cloudformation package --template-file $TEMPLATE --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
-aws cloudformation deploy --template-file out.yml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --template-file out.yml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM
