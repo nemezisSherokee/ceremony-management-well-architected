0876e55c4f27d5246a83b7a33b0f850068adc8e8
JOSEPH HERMANN KAMDOUM NGADJUI <kamdoum_ngadjui@siemens.com>
2021-08-13 11:43:20 +0200

    we now wait until the sonarqube container is running

---
+++ blueprints/java/java-lambda-blue-print/wait_container_up.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+DB_PORT_27017_TCP_ADDR=$1
+DB_PORT_27017_TCP_PORT=$2
+
+while ! curl  "http://$DB_PORT_27017_TCP_ADDR:$DB_PORT_27017_TCP_PORT/api/server/version"
+do
+  echo "$(date) - still trying"
+  sleep 1
+done
+echo "$(date) - connected successfully"
+
+#docker run -d --name sonarqube9000 -p 9008:9000 sonarqube:8.9.0-community
+
