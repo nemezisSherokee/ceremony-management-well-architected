b6eb40672fc66c57a7a2c9ec69b2004c05a570f8
JOSEPH HERMANN KAMDOUM NGADJUI <kamdoum_ngadjui@siemens.com>
2021-09-23 21:36:10 +0200

    customer details now shown

--- web/angular-with-store/src/app/customers/customers.module.ts
+++ web/angular-with-store/src/app/customers/customers.module.ts
@@ -2,14 +2,20 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
-import { CustomersListComponent } from './customers-list/customers-list.component';
-import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
 import { CustomersComponent } from './customers.component';
 import { CustomersRoutingModule } from './customers-routing.module';
+
 import { CustomersEditComponent } from './customers-edit/customers-edit.component';
+import { CustomersDetailComponent } from './customer-detail/customer-detail.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
 
 @NgModule({
     imports: [ CommonModule, SharedModule, CustomersRoutingModule ],
-    declarations: [ CustomersListComponent, FilterTextboxComponent, CustomersComponent, CustomersEditComponent]
+    declarations: [ CustomersListComponent, 
+        FilterTextboxComponent, 
+        CustomersComponent, 
+        CustomersEditComponent,
+    CustomersDetailComponent]
 })
 export class CustomersModule { }
